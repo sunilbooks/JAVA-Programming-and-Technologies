f302a40484bfc8c8aaa8492b276c32da505da428
SuniSahu <books@sunrays.co.in>
2015-04-08 16:48:40 +0530

    Initial Version

--- JavaProgrammingNTech/src/unit5/yr2012/TestHashSet.java
+++ JavaProgrammingNTech/src/unit5/yr2012/TestHashSet.java
@@ -22,15 +22,15 @@ public class TestHashSet {
 	public static void main(String[] args) {
 
 		// Instantiate Set
-		HashSet rollNums = new HashSet();
+		HashSet nums = new HashSet();
 
 		// Addd Elements
-		rollNums.add("One");
-		rollNums.add("Two");
-		rollNums.add("Three");
+		nums.add("One");
+		nums.add("Two");
+		nums.add("Three");
 
 		// Get Iterator and print all elements of Set
-		Iterator it = rollNums.iterator();
+		Iterator it = nums.iterator();
 
 		while (it.hasNext()) {
 			System.out.println(it.next());
